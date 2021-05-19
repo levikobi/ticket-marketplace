1d81ce49d7edc7b3f6db027e824419679fcbb843
Kobi Levi <kobilevi503@gmail.com>
2021-05-19 09:04:22 +0300

    index & app refactor for testing

---
+++ auth/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { loginRouter } from "./routes/login";
+import { logoutRouter } from "./routes/logout";
+import { registerRouter } from "./routes/register";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set("trust proxy", true); // In order to trust Ingress-Nginx
+app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
+
+app.use(currentUserRouter);
+app.use(loginRouter);
+app.use(logoutRouter);
+app.use(registerRouter);
+
+app.all("*", async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { loginRouter } from "./routes/login";
-import { logoutRouter } from "./routes/logout";
-import { registerRouter } from "./routes/register";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set("trust proxy", true); // In order to trust Ingress-Nginx
-app.use(json());
-app.use(cookieSession({ signed: false, secure: true }));
-
-app.use(currentUserRouter);
-app.use(loginRouter);
-app.use(logoutRouter);
-app.use(registerRouter);
-
-app.all("*", async (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
