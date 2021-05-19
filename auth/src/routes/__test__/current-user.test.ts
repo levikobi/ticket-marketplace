6fbf152d0d5c3478d40da01e0dec9d2a281d2752
Kobi Levi <kobilevi503@gmail.com>
2021-05-19 20:41:27 +0300

    add global register function that returns the cookie

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,11 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with details about the current user", async () => {
-    const registerResponse = await request(app)
-        .post("/api/users/register")
-        .send({ email: "test@test.com", password: "password" })
-        .expect(201);
-    const cookie = registerResponse.get("Set-Cookie");
+    const cookie = await global.register();
 
     const response = await request(app)
         .get("/api/users/current-user")
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            register(): Promise<string[]>;
+        }
+    }
+}
 
 let mongo: any;
 
@@ -28,3 +37,15 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+global.register = async () => {
+    const email = "test@test.com";
+    const password = "password";
+
+    const response = await request(app)
+        .post("/api/users/register")
+        .send({ email, password })
+        .expect(201);
+
+    return response.get("Set-Cookie");
+};
