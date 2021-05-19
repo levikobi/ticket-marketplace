7f93c466cfffd531eb409af37e33127e18cfca0b
Kobi Levi <kobilevi503@gmail.com>
2021-05-19 08:15:55 +0300

    logout route

--- auth/src/routes/logout.ts
+++ auth/src/routes/logout.ts
@@ -3,7 +3,9 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/logout", (req, res) => {
-    res.send("hey there");
+    req.session = null;
+
+    res.send({});
 });
 
 export { router as logoutRouter };
