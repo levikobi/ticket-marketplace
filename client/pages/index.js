56d63e1abbe623a8f7a14bca1d220b370cf021be
Kobi Levi <kobilevi503@gmail.com>
2021-05-21 09:13:14 +0300

    use get initial props to fetch current user

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,25 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
     return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+    if (typeof window === "undefined") {
+        // we are on the server! requests should be made
+        // to http://service-name.namespace.svc.cluster.local/...
+        const { data } = await axios.get(
+            "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user",
+            { headers: { Host: "ticketing.dev" } }
+        );
+        return data;
+    } else {
+        // we are on the browser! request can be made
+        // with a base url of ""
+        const { data } = await axios.get("/api/users/current-user");
+        return data;
+    }
+};
+
+export default LandingPage;
