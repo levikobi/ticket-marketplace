92e5e7672eca77f06ebc60849fa637bc40897a66
Kobi Levi <kobilevi503@gmail.com>
2021-05-21 09:16:50 +0300

    pass through the cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,13 +5,13 @@ const LandingPage = ({ currentUser }) => {
     return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
     if (typeof window === "undefined") {
         // we are on the server! requests should be made
         // to http://service-name.namespace.svc.cluster.local/...
         const { data } = await axios.get(
             "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user",
-            { headers: { Host: "ticketing.dev" } }
+            { headers: req.headers }
         );
         return data;
     } else {
