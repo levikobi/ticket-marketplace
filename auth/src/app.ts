9864b414e35438ceabe2d198d3f785202cc850fc
Kobi Levi <kobilevi503@gmail.com>
2021-05-22 10:14:28 +0300

    update auth service import statements to use common package

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,13 +2,12 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@ticket-marketplace/common";
 
 import { currentUserRouter } from "./routes/current-user";
 import { loginRouter } from "./routes/login";
 import { logoutRouter } from "./routes/logout";
 import { registerRouter } from "./routes/register";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.set("trust proxy", true); // In order to trust Ingress-Nginx
