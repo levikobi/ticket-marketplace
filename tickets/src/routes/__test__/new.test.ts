8cd39071866da26db715622d6d5eea9ab6c3e559
Kobi Levi <kobilevi503@gmail.com>
2021-05-23 08:16:27 +0300

    test valid title & price for tickets

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -16,12 +16,36 @@ it("can be accessed if the user is signed in", async () => {
     expect(response.status).not.toEqual(401);
 });
 
-it("can only be accessed if the user is signed in", async () => {
+it("can not be accessed if the user is signed out", async () => {
     await request(app).post("/api/tickets").send({}).expect(401);
 });
 
-it("returns an error if an invalid title is provided", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.register())
+        .send({ title: "", price: 10 })
+        .expect(400);
 
-it("returns an error if an invalid price is provided", async () => {});
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.register())
+        .send({ price: 10 })
+        .expect(400);
+});
+
+it("returns an error if an invalid price is provided", async () => {
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.register())
+        .send({ title: "some title", price: -1 })
+        .expect(400);
+
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.register())
+        .send({ title: "some title" })
+        .expect(400);
+});
 
 it("create a ticket with valid inputs", async () => {});
