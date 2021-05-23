636e92218558a10ad808c233fb9b106f02d1b99b
Kobi Levi <kobilevi503@gmail.com>
2021-05-23 08:35:19 +0300

    define ticket model

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -11,3 +11,36 @@ interface TicketDocument extends TicketAttributes, mongoose.Document {}
 interface TicketModel extends mongoose.Model<TicketDocument> {
     build(attributes: TicketAttributes): TicketModel;
 }
+
+const ticketSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        usedId: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+        },
+    }
+);
+
+ticketSchema.statics.build = (attributes: TicketAttributes) => {
+    return new Ticket(attributes);
+};
+
+const Ticket = mongoose.model<TicketDocument, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
