27751eb1c6a102e085e0c4f701b369614872daf9
Kobi Levi <kobilevi503@gmail.com>
2021-05-25 08:20:58 +0300

    verify ticket can not change with invalid title or price

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -31,5 +31,24 @@ it("returns a 401 if the user does not own the ticket", async () => {
         .expect(401);
 });
 
-it("returns a 400 if the user provides an invalid title or price", async () => {});
+it("returns a 400 if the user provides an invalid title or price", async () => {
+    const cookie = global.register();
+    const response = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", cookie)
+        .send({ title: "some title", price: 40 });
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set("Cookie", cookie)
+        .send({ title: "", price: 20 })
+        .expect(400);
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set("Cookie", cookie)
+        .send({ title: "new valid title", price: -3 })
+        .expect(400);
+});
+
 it("updates the ticket provided valid input", async () => {});
