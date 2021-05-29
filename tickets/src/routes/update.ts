2959812dbc8a3a161ac24ce88af97150d6f7b1b6
Kobi Levi <kobilevi503@gmail.com>
2021-05-29 09:37:40 +0300

    ticket update publishing

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from "express";
+import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import {
     validateRequest,
@@ -7,6 +7,8 @@ import {
     NotAuthorizedError,
 } from "@ticket-marketplace/common";
 import { Ticket } from "../models/ticket";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -33,6 +35,12 @@ router.put(
             price: req.body.price,
         });
         await ticket.save();
+        new TicketUpdatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId,
+        });
 
         res.send(ticket);
     }
