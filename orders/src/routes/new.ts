d37fe89868656e372bce939bdeaa6635e37d7715
Kobi Levi <kobilevi503@gmail.com>
2021-05-30 20:54:43 +0300

    complete new order route, no event publishing yet

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -12,6 +12,8 @@ import { Order } from "../models/order";
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
     "/api/orders",
     requireAuth,
@@ -33,12 +35,21 @@ router.post(
         }
 
         // Calculate an expiration date for this order
+        const expiration = new Date();
+        expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
         // Build the order and save it to the database
+        const order = await Order.build({
+            userId: req.currentUser!.id,
+            status: OrderStatus.Created,
+            expiresAt: expiration,
+            ticket: ticket,
+        });
+        await order.save();
 
         // Publish and event saying that an order was created
 
-        res.send({});
+        res.status(201).send(order);
     }
 );
 
