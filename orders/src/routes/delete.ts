9436941a4658565111e239484a06cdfba97980e6
Kobi Levi <kobilevi503@gmail.com>
2021-05-31 20:03:44 +0300

    publish order cancellation

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,12 +6,14 @@ import {
     requireAuth,
 } from "@ticket-marketplace/common";
 import { Order } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", requireAuth, async (req: Request, res: Response) => {
     const { orderId } = req.params;
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate("ticket");
 
     if (!order) {
         throw new NotFoundError();
@@ -22,6 +24,13 @@ router.delete("/api/orders/:orderId", requireAuth, async (req: Request, res: Res
 
     order.status = OrderStatus.Cancelled;
     await order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id,
+        },
+    });
 
     res.status(204).send(order);
 });
