a2ca75425a230c71baf8f670c6fd6c7f84bf9c0e
Kobi Levi <kobilevi503@gmail.com>
2021-05-31 07:48:05 +0300

    test success case of creating a new order

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -34,4 +34,16 @@ it("return an error if the ticket is already reserved", async () => {
         .expect(400);
 });
 
-it("reserves a ticket", async () => {});
+it("reserves a ticket", async () => {
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 20,
+    });
+    await ticket.save();
+
+    await request(app)
+        .post("/api/orders")
+        .set("Cookie", global.register())
+        .send({ ticketId: ticket.id })
+        .expect(201);
+});
