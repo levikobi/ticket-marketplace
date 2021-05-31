eb7366936e9574dbe98d682bc0c15b7c52c91b5c
Kobi Levi <kobilevi503@gmail.com>
2021-05-31 07:44:00 +0300

    assert reserved tickets

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,5 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Order, OrderStatus } from "../../models/order";
+import { Ticket } from "../../models/ticket";
 
 it("return an error if the ticket does not exists", async () => {
     const ticketId = global.generateId();
@@ -10,6 +12,26 @@ it("return an error if the ticket does not exists", async () => {
         .expect(404);
 });
 
-it("return an error if the ticket is already reserved", async () => {});
+it("return an error if the ticket is already reserved", async () => {
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 20,
+    });
+    await ticket.save();
+
+    const order = Order.build({
+        ticket,
+        userId: global.generateId(),
+        status: OrderStatus.Created,
+        expiresAt: new Date(),
+    });
+    await order.save();
+
+    await request(app)
+        .post("/api/orders")
+        .set("Cookie", global.register())
+        .send({ ticketId: ticket.id })
+        .expect(400);
+});
 
 it("reserves a ticket", async () => {});
