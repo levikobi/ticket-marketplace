bdf0e173b0c0a20918882f2d459364a56aab8899
Kobi Levi <kobilevi503@gmail.com>
2021-06-02 08:40:46 +0300

    verify ticket model implements optimistic concurrency control

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,3 +1,25 @@
 import { Ticket } from "../ticket";
 
-it("implements optimistic concurrency control", async () => {});
+it("implements optimistic concurrency control", async (done) => {
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 5,
+        userId: global.generateId(),
+    });
+    await ticket.save();
+
+    const instance1 = await Ticket.findById(ticket.id);
+    const instance2 = await Ticket.findById(ticket.id);
+
+    instance1!.set({ price: 10 });
+    instance2!.set({ price: 15 });
+
+    await instance1!.save();
+    try {
+        await instance2!.save();
+    } catch (err) {
+        return done();
+    }
+
+    throw new Error("Should not reach this point");
+});
