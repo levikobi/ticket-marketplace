adce46457a49ae02595d5cff9fc8e36dd09ef18b
Kobi Levi <kobilevi503@gmail.com>
2021-06-03 08:38:45 +0300

    verify ticket created listener creates and saves the ticket

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,37 @@
+import { Message } from "node-nats-streaming";
+import { TicketCreatedEvent } from "@ticket-marketplace/common";
+import { natsWrapper } from "../../../nats-wrapper";
+import { TicketCreatedListener } from "../ticket-created-listener";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+    const listener = new TicketCreatedListener(natsWrapper.client);
+
+    const data: TicketCreatedEvent["data"] = {
+        version: 0,
+        id: global.generateId(),
+        title: "concert",
+        price: 10,
+        userId: global.generateId(),
+    };
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn(),
+    };
+
+    return { listener, data, msg };
+};
+
+it("creates and saves a ticket", async () => {
+    const { listener, data, msg } = await setup();
+    await listener.onMessage(data, msg);
+
+    const ticket = await Ticket.findById(data.id);
+
+    expect(ticket).toBeDefined();
+    expect(ticket!.title).toEqual(data.title);
+    expect(ticket!.price).toEqual(data.price);
+});
+
+it("acknowledges the message", async () => {});
