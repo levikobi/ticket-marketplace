735edd42c523ab472dede4c44b110adcf69efd39
Kobi Levi <kobilevi503@gmail.com>
2021-06-05 09:44:01 +0300

    setup for testing reservation of a ticket

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,35 @@
+import { OrderCreatedEvent, OrderStatus } from "@ticket-marketplace/common";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCreatedListener } from "../order-created-listener";
+
+const setup = async () => {
+    const listener = new OrderCreatedListener(natsWrapper.client);
+
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 99,
+        userId: global.generateId(),
+    });
+    await ticket.save();
+
+    const data: OrderCreatedEvent["data"] = {
+        id: global.generateId(),
+        version: 0,
+        status: OrderStatus.Created,
+        userId: global.generateId(),
+        expiresAt: "abcde",
+        ticket: {
+            id: ticket.id,
+            price: ticket.price,
+        },
+    };
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn(),
+    };
+
+    return { listener, ticket, data, msg };
+};
