c34828454eed0116377cf5f8566b07c1d7f98834
Kobi Levi <kobilevi503@gmail.com>
2021-06-08 08:43:26 +0300

    create a payments model

---
+++ payments/src/models/payment.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+interface PaymentAttributes {
+    orderId: string;
+    stripeId: string;
+}
+
+interface PaymentDocument extends PaymentAttributes, mongoose.Document {}
+
+interface PaymentModel extends mongoose.Model<PaymentDocument> {
+    build(attributes: PaymentAttributes): PaymentDocument;
+}
+
+const paymentSchema = new mongoose.Schema(
+    {
+        orderId: {
+            required: true,
+            type: String,
+        },
+        stripeId: {
+            required: true,
+            type: String,
+        },
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+        },
+    }
+);
+
+paymentSchema.statics.build = (attributes: PaymentAttributes) => {
+    return new Payment(attributes);
+};
+
+const Payment = mongoose.model<PaymentDocument, PaymentModel>("Payment", paymentSchema);
+
+export { Payment };
